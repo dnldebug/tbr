d1bae110eee3807342c2311ffbc88a048b735961
__dnl_ <danillopedro2020@gmail.com>
2025-11-21 14:16:08 -0300

    Update Rota_1.py

--- Rota_1.py
+++ Rota_1.py
@@ -2,10 +2,18 @@ from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
-from consts import Open, close, low, mid, deposit, cancela_esquerda, cancela_direita, volta_direita, volta_esquerda
-from Drive_Train import claw, andar, turn, rotate, cancela
+from pybricks.tools import wait, StopWatch
+from Drive_Train import andar, turn, rotate
+from consts import Open, close, low, mid, upper, deposit, cancela_esquerda, volta_esquerda, cancela_direita, volta_direita, direita_direto, esquerda_direto
+
 
 hub = PrimeHub()
+
+# rota 1
+#andar(25)
+#close()    
+#rotate(-90)
+#andar(30)
 
 
 # SAI DA BASE
@@ -13,65 +21,256 @@ def route_I():
     andar(25)
     close()
     rotate(-90)
-    andar(30)
-    cor_emergency()
+    andar(31)
+    
 # VAI PARA O TREM
 def train_I():
     rotate(-90)
-    andar(16)
-    rotate(90)
-    andar(18)
-    detect_train_I()
+    andar(16.6)
+    rotate(91)
+    cancela_esquerda()
+    andar(32)
+    wait(600)
+    
+    
 
 #caminho até a caixa verde
 def route_II():
-    rotate(90)
-    andar(45)
-    rotate(90)
-    andar(20)
+    rotate(65)
+    andar(58.5)
+    wait(100)
+    rotate(97)
+    wait(300)
+    Open()
+    wait(100)
+    andar(17.5)
+    wait(100)
+    close()
 
-
+#Caminho para fazer o trainII 
 def route_III():
-    andar(-40)
-    rotate(90)
-    cancela_direita()
+    andar(-22)
+    wait(300)
+    rotate(86)
+    wait(300)
+    direita_direto()
+    wait(100)
+    andar(-18)
+    wait(200)
+    rotate(23)
+    wait(100)
+    esquerda_direto()
+    wait(400)
+    andar(3)
+    
 
 #caminho para depositar a caixa verde
-def boxG():
-    rotate(-90)
-    andar(30)
-    rotate(90)
-    andar(5)
+def boxG_azul():
 
-#caminho até a caixa azul
-def route_IV():
-    andar(-30)
+    wait(200)
     rotate(-90)
-    andar(60)
-    rotate(-90)
-
-#caixa azul
-def boxB():
-    andar(15)
-    low()
-    andar(-15)
+    andar(16)
     rotate(90)
+    wait(200)
+    andar(45)
+    wait(100)
+    rotate(-90)
+    wait(100)
+    andar(38)
+    wait(100)
+    rotate(90)
+    andar(18.5)
     Open()
+    wait(500)
+    andar(-14.5)
+    rotate(-90)
 
+
+def boxG_verde():
+
+    wait(200)
+    rotate(-90)
+    andar(16)
+    rotate(90)
+    wait(200)
+    andar(45)
+    wait(100)
+    rotate(-90)
+    wait(100)
+    andar(35)
+    wait(100)
+    rotate(90)
+    andar(16)
+    Open()
+    wait(500)
+    andar(-14.5)
+    rotate(-90)
+
+
+def boxG_amarelo():
+
+    wait(400)
+    rotate(-90)
+    andar(17)
+    rotate(90)
+    wait(200)
+    andar(44)
+    wait(100)
+    rotate(-90)
+    wait(100)
+    andar(37.5)
+    wait(100)
+    rotate(90)
+    andar(18.5)
+    Open()
+    wait(500)
+    andar(-14.5)
+    rotate(-90)
+
+
+def boxG_cinza():
+
+    cancela_esquerda()
+    wait(400)
+    rotate(-25)
+    andar(20)
+    rotate(25)
+    wait(200)
+    andar(30)
+    wait(100)
+    rotate(-90)
+    wait(100)
+    volta_esquerda()
+    wait(200)
+    andar(45)
+    wait(100)
+    rotate(90)
+    andar(16)
+    Open()
+    wait(500)
+    andar(-17)
+    rotate(-90)
+    
+
+#caminho até o trainIII
+def route_IV():
+    close()
+    andar(65) 
+    wait(100)
+    
+#fazer trainIII
 def train_III():
     rotate(90)
-    andar(20)
-    rotate(90)
+    andar(18)
+    upper()
+    rotate(-90)
+    andar(13)
     cancela_direita()
+    andar(4)
+    wait(300)
+    
 
+
+#caminho até o trainIV
+def route_V():
+    rotate(-11)
+    andar(34)
+    rotate(-79)
+    cancela_direita()
+    andar(18)
+
+#fazer trainIV
 def train_IV():
     rotate(-90)
-    andar(20)
+    andar(18.5)
+    
+
+def route_VI_azul():
+    rotate(-20)
+    andar(48)
+    rotate(-66)
+    andar(33)
+    rotate(-90)
+    wait(300)
+    andar(-6)
+    wait(200)
+    Open()
+    wait(200)
+    andar(6)
+    wait(200)
+    low()
+    andar(-3.5)
+    close()
+
+def route_VI_amarelo():
+    rotate(-20)
+    andar(48)
+    rotate(-66)
+    andar(32)
+    rotate(-90)
+    wait(300)
+    andar(-6)
+    wait(200)
+    Open()
+    wait(200)
+    andar(7)
+    wait(200)
+    low()
+    andar(-3.5)
+    close()
+
+def route_VI_verde():
+    rotate(-20)
+    andar(48)
+    rotate(-66)
+    andar(32)
+    rotate(-90)
+    wait(300)
+    andar(-6)
+    wait(200)
+    Open()
+    wait(200)
+    andar(7)
+    wait(200)
+    low()
+    andar(-3.5)
+    close()
+
+def route_VI_cinza():
+    rotate(-20)
+    andar(48)
+    rotate(-66)
+    andar(32)
+    rotate(-90)
+    wait(300)
+    andar(-6)
+    wait(200)
+    Open()
+    wait(200)
+    andar(7)
+    wait(200)
+    low()
+    andar(-3.5)
+    close()
+
+
+def boxB_():
+    rotate(90)
+    andar(-41)
+    rotate(90)
+    andar(-50)
+    rotate(90)
+    andar(-40.1)
+    Open()
+    wait(100)
+    
+
+    
 
 #estacionar
 def park():
-    rotate(-90)
-    andar(65)
-    rotate(90)
-    andar(-5)
+    andar(-15.55)
     deposit()
+    rotate(-90)
+    andar(49.5)
+
